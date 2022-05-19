94a50dfb1aca7040a18d590fe831234b6eac387c
Gordon Keller <Gordon Keller>
2022-05-19 09:57:03 -0700

    stopped tracking unnecessary stuff in repo (build, devel)

--- build/uav_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gordon_l1804/control_ws/src/uav_control
-# Build directory: /home/gordon_l1804/control_ws/build/uav_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
